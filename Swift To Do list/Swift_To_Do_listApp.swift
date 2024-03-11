91b63d23f83f5b4b6ebc68ced17becd32bd65f00
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-03-11 10:25:43 -0700

    To do list assignment

---
+++ Swift To Do list/Swift_To_Do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_To_Do_listApp.swift
+//  Swift To Do list
+//
+//  Created by StudentAM on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_To_Do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
